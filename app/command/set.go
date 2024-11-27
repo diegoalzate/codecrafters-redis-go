0a2571f89cd0b64ff71d4df6279f50e5a13fb428
Diego Alzate <diego35.da@gmail.com>
2024-11-27 22:32:38 +0000

    codecrafters submit [skip ci]

--- app/command/set.go
+++ app/command/set.go
@@ -3,19 +3,43 @@ package command
 import (
 	"errors"
 	"fmt"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/codecrafters-io/redis-starter-go/app/resp"
 	"github.com/codecrafters-io/redis-starter-go/app/store"
 )
 
 func RunSet(store *store.Store, args []string) (string, error) {
-	if len(args) != 2 {
-		fmt.Println(fmt.Printf("expected 2 args, received: %v", args))
-		return "", errors.New("expected 2 args")
+	if len(args) < 2 {
+		fmt.Println(fmt.Printf("expected at least 2 args, received: %v", args))
+		return "", errors.New("expected at least 2 args")
 	}
 
 	key := args[0]
 	val := args[1]
+
+	if len(args) > 2 {
+		px := strings.ToLower(args[2])
+
+		if px == "px" {
+			ttlStr := args[3]
+			ttlVal, err := strconv.Atoi(ttlStr)
+
+			if err != nil {
+				return "", err
+			}
+
+			ttl := time.Duration(ttlVal) * time.Millisecond
+
+			store.SetWithExpiry(key, val, ttl)
+
+			return resp.OK, nil
+		}
+
+		return "", errors.New("failed to parse expiry")
+	}
 
 	store.Set(key, val)
 
--- app/store/store.go
+++ app/store/store.go
@@ -1,20 +1,57 @@
 package store
 
+import "time"
+
+type Expiry struct {
+	Enabled  bool
+	ExpireAt time.Time
+}
+
+type StoreValue struct {
+	String string
+	Expiry Expiry
+}
+
 type Store struct {
-	values map[string]string
+	values map[string]StoreValue
 }
 
 func NewStore() Store {
 	return Store{
-		values: make(map[string]string),
+		values: make(map[string]StoreValue),
 	}
 }
 
 func (s *Store) Get(key string) (string, bool) {
 	val, exists := s.values[key]
-	return val, exists
+
+	// check if has expired
+	now := time.Now()
+
+	if val.Expiry.Enabled && val.Expiry.ExpireAt.Before(now) {
+		return "", false
+	}
+
+	return val.String, exists
 }
 
 func (s *Store) Set(key string, val string) {
-	s.values[key] = val
+	s.values[key] = StoreValue{
+		String: val,
+		Expiry: Expiry{},
+	}
+}
+
+func (s *Store) SetWithExpiry(key string, val string, ttl time.Duration) {
+	expireAt := time.Now().Add(ttl)
+
+	storeValue := StoreValue{
+		String: val,
+		Expiry: Expiry{
+			Enabled:  true,
+			ExpireAt: expireAt,
+		},
+	}
+
+	s.values[key] = storeValue
 }
